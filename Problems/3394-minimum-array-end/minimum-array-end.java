3493dda3fee044802c9d9a3db9b9ed19c3a6faf9
Rakeshram27 <136475235+Rakeshram27@users.noreply.github.com>
2024-04-28 21:05:01 +0530

    Time: 2 ms (100.00%) | Memory: 40.5 MB (100.00%) - LeetSync

--- Problems/3394-minimum-array-end/minimum-array-end.java
+++ Problems/3394-minimum-array-end/minimum-array-end.java
@@ -1,9 +1,28 @@
 class Solution {
     public long minEnd(int n, int x) {
         long val = x;
+        String str = Integer.toBinaryString(n - 1);
+        int idx = 0;
+        long ones = 1;
+        for(int i = str.length() - 1;i >= 0;i--){
+            if(str.charAt(i) == '0'){
+                while((val & (ones<<idx)) != 0){
+                    idx++;
+                }
+                idx++;
+            }else{
+                while((val & (ones<<idx)) != 0){
+                    idx++;
+                }
+                val = val | (ones<<idx);
+                idx++;
+            }
+        }
+        return val;
+        /*long val = x;
         while(--n > 0) {
             val = (val + 1) | x;
         }
-        return val;
+        return val;*/
     }
 }
