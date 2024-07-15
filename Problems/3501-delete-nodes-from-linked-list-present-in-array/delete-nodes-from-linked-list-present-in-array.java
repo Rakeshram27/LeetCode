a95e93cd51ef1fbaa70d3f2ed72eceb665ad40fd
Rakeshram27 <136475235+Rakeshram27@users.noreply.github.com>
2024-07-15 14:48:26 +0530

    Time: 21 ms (50.00%) | Memory: 64.3 MB (50.00%) - LeetSync

--- Problems/3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ Problems/3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -10,43 +10,25 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-         ListNode dummy = new ListNode(-1);  
-        ListNode prev = dummy;  
-        dummy.next = head;  
-        
-        HashSet<Integer> set = new HashSet<>();  
-        for (int num : nums) {  
-            set.add(num);  
-        }  
-        
-        while (prev.next != null) {  
-            if (set.contains(prev.next.val)) {  
-                prev.next = prev.next.next;  
-            } else {  
-                prev = prev.next;  
-            }  
-        }  
-        
-        return dummy.next; 
-        // Set<Integer> st = new HashSet<>();
-        // ListNode dummy = new ListNode(0);
-        // dummy.next = head;
-        // ListNode prev = dummy;
-        // ListNode curr = head;
-        // for(int n : nums) {
-        //     st.add(n);
-        // }
-        // while(curr != null) {
-        //     if(st.contains(curr.val)) {
-        //         prev.next = curr.next;
-        //     }
-        //     else {
-        //         prev = curr;
-        //     }
-        //     curr = curr.next;
-        // }
-        // prev.next = null;
-        // return dummy.next;
+        Set<Integer> st = new HashSet<>();
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+        for(int n : nums) {
+            st.add(n);
+        }
+        while(curr != null) {
+            if(st.contains(curr.val)) {
+                prev.next = curr.next;
+            }
+            else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        prev.next = null;
+        return dummy.next;
         // ListNode s = new ListNode(0);
         // s.next = head;
         // ListNode curr = s;
