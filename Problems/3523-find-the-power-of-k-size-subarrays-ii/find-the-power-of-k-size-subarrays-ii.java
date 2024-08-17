d5d841a73e7ee9db9539da165e3116020de86885
Rakeshram27 <136475235+Rakeshram27@users.noreply.github.com>
2024-08-17 22:33:49 +0530

    Time: 3 ms (100.00%) | Memory: 64.4 MB (50.00%) - LeetSync

---
+++ Problems/3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        if(k == 1) return nums;
+        int[] res = new int[nums.length + 1 - k];
+        Arrays.fill(res, - 1);
+        int cnt = 1;
+        for(int i = 0;i < nums.length - 1;i++) {
+            if(nums[i + 1] - nums[i] == 1) {
+                cnt++;
+            }
+            else {
+                cnt = 1;
+            }
+            if(cnt >= k) {
+                res[i - k + 2] = nums[i + 1];
+            }
+        }
+        return res;
+    }
+}
