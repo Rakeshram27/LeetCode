b427557af1ea419c4a51cd22133e3f5198656f4a
Rakeshram27 <136475235+Rakeshram27@users.noreply.github.com>
2025-03-10 21:31:13 +0530

    Time: 40 ms (86.73%) | Memory: 53 MB (26.16%) - LeetSync

---
+++ Problems/2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a = 0, b = 1;
+    while(true) {
+        yield a;
+        b = a + b;
+        a = b - a;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
